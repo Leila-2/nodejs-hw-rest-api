16962a324e855d4df276cfa7314b9f5eeee97516
Leila-2 <liliya.yavorskaya17@gmail.com>
2022-01-28 23:33:16 +0200

    Done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,87 @@
+/* eslint-disable new-cap */
 const express = require('express')
+const createError = require("http-errors");
+const contacts = require('../../models/contacts')
+
+const Joi = require("joi");
+
+const contsctSchema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().email()
+});
 
 const router = express.Router()
-
+// GET /api/contacts
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contacts.listContacts()
+    res.json(result)
+  } catch (error) {
+    next(error);
+  }
+
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id)
+    if (!result) {
+      // eslint-disable-next-line new-cap
+      throw new createError(404, "Not found");
+    }
+    res.json(result)
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = contsctSchema.validate(req.body)
+    if (error) {
+      // eslint-disable-next-line new-cap
+      throw new createError(400, error.message)
+    }
+    const { name, email, phone } = req.body
+    const result = await contacts.addContact(name, email, phone)
+    res.status(201).json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId)
+    if (!result) {
+      throw new createError(404, "Not found")
+    }
+    res.json({ message: "Contact deleted" })
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = contsctSchema.validate(req.body)
+    if (error) {
+      throw new createError(400, error.message)
+    }
+    const { contactId } = req.params
+    const { name, email, phone } = req.body
+    const result = await contacts.updateContact(contactId, name, email, phone)
+    if (!result) {
+      throw new createError(404, "Not found")
+    }
+    res.json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
