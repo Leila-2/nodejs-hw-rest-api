ef6c95ff2b6de0a1f438975833ecc775cdfdacea
Leila-2 <liliya.yavorskaya17@gmail.com>
2022-02-17 19:37:54 +0200

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -14,8 +14,10 @@ const router = new Router()
 
 router.get('/', authenticate, async (req, res, next) => {
   try {
-
     const { page = 1, limit = 20 } = req.query;
+    if (isNaN(page) || isNaN(limit)) {
+      throw new createError(400, 'Params limit and page must be a number')
+    }
     const skip = (page - 1) * limit;
     const { _id } = req.user
     const result = await Contact.find({ owner: _id }, '-createdAt', { skip, limit: +limit }).populate("owner")
@@ -26,13 +28,14 @@ router.get('/', authenticate, async (req, res, next) => {
 
 })
 
-router.get('/:contactId', async (req, res, next) => {
+router.get('/:contactId', authenticate, async (req, res, next) => {
   try {
     const { contactId } = req.params;
+    const ownerId = req.user._id
     if (!mongoose.Types.ObjectId.isValid(contactId)) {
       throw new createError(400, "invalid ID")
     }
-    const result = await Contact.findById(contactId);
+    const result = await Contact.findById({ _id: contactId, owner: ownerId });
 
     if (!result) {
       throw new createError(404, "Not found");
@@ -59,13 +62,14 @@ router.post('/', authenticate, async (req, res, next) => {
   }
 })
 
-router.delete('/:contactId', async (req, res, next) => {
+router.delete('/:contactId', authenticate, async (req, res, next) => {
   try {
     const { contactId } = req.params;
+    const ownerId = req.user._id
     if (!mongoose.Types.ObjectId.isValid(contactId)) {
       throw new createError(400, "invalid ID")
     }
-    const result = await Contact.findByIdAndDelete(contactId)
+    const result = await Contact.findByIdAndDelete({ _id: contactId, owner: ownerId })
     if (!result) {
       throw new createError(404, "Not found")
     }
@@ -75,17 +79,18 @@ router.delete('/:contactId', async (req, res, next) => {
   }
 })
 
-router.put('/:contactId', async (req, res, next) => {
+router.put('/:contactId', authenticate, async (req, res, next) => {
   try {
     const { error } = schemas.add.validate(req.body)
     if (error) {
       throw new createError(400, error.message)
     }
     const { contactId } = req.params
+    const ownerId = req.user._id
     if (!mongoose.Types.ObjectId.isValid(contactId)) {
       throw new createError(400, "invalid ID")
     }
-    const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true })
+    const result = await Contact.findByIdAndUpdate({ _id: contactId, owner: ownerId }, req.body, { new: true })
     if (!result) {
       throw new createError(404, "Not found")
     }
@@ -95,17 +100,18 @@ router.put('/:contactId', async (req, res, next) => {
   }
 })
 
-router.patch('/:contactId/favorite', async (req, res, next) => {
+router.patch('/:contactId/favorite', authenticate, async (req, res, next) => {
   try {
     const { error } = schemas.update.validate(req.body)
     if (error) {
       throw new createError(400, "missing field favorite")
     }
     const { contactId } = req.params
+    const ownerId = req.user._id
     if (!mongoose.Types.ObjectId.isValid(contactId)) {
       throw new createError(400, "invalid ID")
     }
-    const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true })
+    const result = await Contact.findByIdAndUpdate({ _id: contactId, owner: ownerId }, req.body, { new: true })
     if (!result) {
       throw new createError(404, "Not found")
     }
